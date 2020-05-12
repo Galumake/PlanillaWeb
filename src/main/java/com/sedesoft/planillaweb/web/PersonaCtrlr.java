80e2dd5d67e7225c6788c1ee98b550dd60e428de
Alonso Umana <alfonso.uma@gmail.com>
2020-05-11 21:34:31 -0600

    otra parte del controller

--- src/main/java/com/sedesoft/planillaweb/web/PersonaCtrlr.java
+++ src/main/java/com/sedesoft/planillaweb/web/PersonaCtrlr.java
@@ -1,5 +1,21 @@
 package com.sedesoft.planillaweb.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sedesoft.planillaweb.service.PersonaSrv;
+
+@Controller
 public class PersonaCtrlr {
+	
+	@Autowired
+	PersonaSrv personaSrv;
+	
+	@GetMapping("/config/person/employee/show")
+	public String personaShow(Model model) {
+		return  "/config/persona/colaboradores";
+	}
 
 }
